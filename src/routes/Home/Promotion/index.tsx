567d05899f289bb16334c998bc625b55472b9591
Adriano Abreu <adriano.luis.machado2013@hotmail.com>
2025-06-12 16:59:37 -0300

    ReactJS - Exercicio 3 - Implementacao de Links, NavLinks, Rotas e isActive

--- src/routes/Home/Promotion/index.tsx
+++ src/routes/Home/Promotion/index.tsx
@@ -1,7 +1,25 @@
-export default function Promotion(){
+import { Link } from "react-router-dom";
+import Button from "../../../components/Button";
+import Card from "../../../components/Card";
+
+export default function Promotion() {
     return (
         <main>
-            <h2>Pagina de Promoção</h2>
+            <section>
+                <div className="container">
+                    <div className="mt30 mb30">
+                        <Card title="Pagina de promoção" />
+                    </div>
+                    <div className="dflex">
+                        <Link to="/sub">
+                            <Button text="Quero participar" />
+                        </Link>
+
+                    </div>
+
+                </div>
+
+            </section>
         </main>
     );
 }
